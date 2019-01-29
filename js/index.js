d4c0bc2d55b597e2a0361a160bea64baad394700
Joel Bartlett <joel@murbar.com>
2019-01-29 16:09:41 -0500

    Change p color on click, reset on dbl click

--- js/index.js
+++ js/index.js
@@ -10,3 +10,17 @@ contentImgs.forEach(img => {
     img.addEventListener('mouseenter', e => e.target.classList.add('tilt'));
     img.addEventListener('mouseleave', e => e.target.classList.remove('tilt'));
 });
+
+function randomHsl() {
+    return `hsla(${(Math.random() * 360)}, 100%, 50%, 1)`;
+}
+
+const paragraphs = document.querySelectorAll('p');
+paragraphs.forEach(p => {
+    p.addEventListener('dblclick', e => {
+        e.target.style.color = 'black';
+    });
+    p.addEventListener('click', e => {
+        e.target.style.color = randomHsl();
+    });
+});
