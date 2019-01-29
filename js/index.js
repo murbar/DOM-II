5d2dc05ba2982aa58982a98a2f089162fdac897b
Joel Bartlett <joel@murbar.com>
2019-01-29 16:09:20 -0500

    Tilt img on mouse hover

--- js/index.js
+++ js/index.js
@@ -5,3 +5,8 @@ nav.addEventListener('click', e => {
     console.log('nav link clicked');
 })
 
+const contentImgs = document.querySelectorAll('.content-section img');
+contentImgs.forEach(img => {
+    img.addEventListener('mouseenter', e => e.target.classList.add('tilt'));
+    img.addEventListener('mouseleave', e => e.target.classList.remove('tilt'));
+});
